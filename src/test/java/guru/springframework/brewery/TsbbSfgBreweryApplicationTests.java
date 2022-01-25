6d01c0b81499689cf4423eabf3e431e270e66780
tosumaso <wakuwaku5712@gmail.com>
2022-01-25 15:44:35 +0900

    first commit

--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//@SpringBootTest : @SpringBootApplicationのあるメインクラスを見る、@ComponentScanの範囲のConfigクラスをcontextに登録。context全体を読み込むため遅い。
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TsbbSfgBreweryApplicationTests {
