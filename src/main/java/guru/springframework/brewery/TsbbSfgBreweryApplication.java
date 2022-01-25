6ad7040e99cd87b5ae16f4726eef9df37ea9fcac
tosumaso <wakuwaku5712@gmail.com>
2022-01-25 21:50:24 +0900

    circleci test

--- src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
+++ src/main/java/guru/springframework/brewery/TsbbSfgBreweryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TsbbSfgBreweryApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TsbbSfgBreweryApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TsbbSfgBreweryApplication.class, args);
+	}
 
 }
--- src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
+++ src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -1,17 +1,14 @@
 package guru.springframework.brewery;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-//@SpringBootTest : @SpringBootApplicationのあるメインクラスを見る、@ComponentScanの範囲のConfigクラスをcontextに登録。context全体を読み込むため遅い。
-@RunWith(SpringRunner.class)
+//@SpringBootTest : @SpringBootApplicationのあるメインクラスを見る、@ComponentScanの範囲のConfigクラスをcontextに登録。context全体を読み込むため遅い
 @SpringBootTest
 public class TsbbSfgBreweryApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
